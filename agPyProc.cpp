4045eef0e0d544458350245a42d26ced4adc4766
Gaetan Guidet <gatgui@gmail.com>
2014-09-29 17:45:05 +0900

    set sys.dont_write_bytecode to True, renamed fixSysPath to setup

--- agPyProc.cpp
+++ agPyProc.cpp
@@ -162,14 +162,15 @@ private:
     msInstance = NULL;
   }
   
-  void fixSysPath()
+  void setup()
   {
     // This code was originally using sys.prefix
     // But on windows, sys.prefix doesn't necessarily matches the directory of 
     //   the actually used python DLL
     // What we want here is to be sure to use the binary modules shipped with
     //   the python DLL in use
-    static const char *scr = "import sys, os;\n\
+    static const char *scr = "import sys, os\n\
+sys.dont_write_bytecode = True\n
 if sys.platform == \"win32\":\n\
   dlls = os.path.join(os.path.split(os.path.dirname(os.__file__))[0], \"DLLs\")\n\
   if dlls in sys.path:\n\
